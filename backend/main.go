27db61f4d06688a4e1e5dd846a137b62ffb53d62
Stepan Fedosov <zollidan@yandex.ru>
2025-04-08 21:58:28 +0300

    feat: add PostgreSQL support and user management endpoints

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"time"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var db *gorm.DB
 
 type User struct {
 	ID           uuid.UUID `gorm:"type:uuid;primaryKey"`
@@ -43,17 +48,36 @@ type Favorite struct {
 	SavedAt time.Time `gorm:"autoCreateTime"`
 }
 
-func InitDatabase() {
-	var err error
-	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
+type UserSchema struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
-	err = DB.AutoMigrate(&User{}, &UserPhoto{}, &Favorite{})
-	if err != nil {
-		log.Fatal("Failed to migrate database:", err)
-	}
+type UserLoginSchema struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+func InitDatabase() {
+
+	if err := godotenv.Load("../.env"); err != nil {
+        log.Print("No .env file found")
+    }
+
+	host := os.Getenv("DATABASE_HOST")
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	dbname := os.Getenv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", host, user, password, dbname)
+	var err error
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	db.AutoMigrate(&User{}, &UserPhoto{}, &Favorite{})
 }
 
 func HashPassword(password string) (string, error) {
@@ -66,14 +90,56 @@ func CheckPasswordHash(password, hash string) bool {
     return err == nil
 }
 
-func createUser(c *fiber.Ctx) error {
-	type Request struct {
-		Username string `json:"username"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
+func getOneUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	
+	var user User 
+	if err := db.First(&user, "id = ?", id).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"error": "User not found",
+		})
+	} 
+
+	return c.JSON(user)
+}
+
+func updateUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	return c.JSON(fiber.Map{
+		"message": "User updated successfully",
+		"id": id,
+	})
+	
+}
+
+func deleteUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	var user User
+
+	if err := db.Delete(&user, "id = ?", id).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": "Error deleting user",
+		})
 	}
 
-	var body Request
+	return c.JSON(fiber.Map{
+		"message": "User deleted successfully",
+	})
+}
+
+func getUsers(c *fiber.Ctx) error {
+	var users []User
+	if err := db.Find(&users).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Could not get users"})
+	}
+	return c.JSON(users)
+}
+
+func registerUser(c *fiber.Ctx) error {
+
+	var body UserSchema
 	if err := c.BodyParser(&body); err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
@@ -88,33 +154,75 @@ func createUser(c *fiber.Ctx) error {
 		CreatedAt:    time.Now(),
 	}
 
-	if err := DB.Create(&user).Error; err != nil {
+	if err := db.Create(&user).Error; err != nil {
 		return c.Status(500).JSON(fiber.Map{"error": "Could not create user"})
 	}
 
 	return c.JSON(user)
 }
 
-func getUsers(c *fiber.Ctx) error {
-	var users []User
-	if err := DB.Find(&users).Error; err != nil {
-		return c.Status(500).JSON(fiber.Map{"error": "Could not get users"})
-	}
-	return c.JSON(users)
-}
+func login(c *fiber.Ctx) error {
 
-func main() {
+	var body UserLoginSchema
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	var user User 
+	if err := db.First(&user, "email = ?", body.Email).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"error": "User not found",
+		})
+	} 
+
+	isPasswordValid := CheckPasswordHash(body.Password, user.PasswordHash)
+	if !isPasswordValid {
+		return c.Status(401).JSON(fiber.Map{
+			"error": "Invalid password",
+		})
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+	 "email":  user.Email,
+	 "exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+	
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+   
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+	 return c.SendStatus(fiber.StatusInternalServerError)
+	}
+   
+	return c.JSON(fiber.Map{"token": t})
+   }
+
+   func main() {
 	InitDatabase()
 
 	app := fiber.New()
 
 	app.Get("/api/hello", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message": "Hello from Go backend!",
+			"message": "Hello, this is ImgHost API!",
+			"version": "0.0.1",
 		})
 	})
 
-	app.Post("/api/users", createUser)
+	// Открытые маршруты
+	app.Post("/api/auth/login", login)
+	app.Post("/api/auth/register", registerUser)
+
+
+	// Мидделваре
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
+
+	// Защищённые маршруты
 	app.Get("/api/users", getUsers)
 	app.Get("/api/users/:id", getOneUser)
 	app.Patch("/api/users/:id", updateUser)
