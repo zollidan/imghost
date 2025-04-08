3ed81c8cab744550263a5778256c0af62f3bfc1c
Stepan Fedosov <zollidan@yandex.ru>
2025-04-08 16:24:57 +0300

    feat: implement user management with database integration

--- backend/go.mod
+++ backend/go.mod
@@ -3,16 +3,27 @@ module github.com/zollidan/imghost
 go 1.24.1
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/google/uuid v1.6.0
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.6 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,111 @@
-// main.go
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+type User struct {
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Username     string    `gorm:"uniqueIndex;not null"`
+	Email        string    `gorm:"uniqueIndex;not null"`
+	PasswordHash string    `gorm:"not null"`
+	CreatedAt    time.Time
+
+	UserPhotos []UserPhoto
+	Favorites  []Favorite
+}
+
+type UserPhoto struct {
+	ID         uuid.UUID `gorm:"type:uuid;primaryKey"`
+	UserID     uuid.UUID `gorm:"type:uuid;not null"`
+	User       User      `gorm:"foreignKey:UserID"`
+	URL        string    `gorm:"not null"`
+	Title      string
+	UploadedAt time.Time `gorm:"autoCreateTime"`
+}
+
+type Favorite struct {
+	ID      uuid.UUID `gorm:"type:uuid;primaryKey"`
+	UserID  uuid.UUID `gorm:"type:uuid;not null"`
+	User    User      `gorm:"foreignKey:UserID"`
+	PhotoID uuid.UUID `gorm:"type:uuid;not null"`
+	Photo   UserPhoto `gorm:"foreignKey:PhotoID"`
+	SavedAt time.Time `gorm:"autoCreateTime"`
+}
+
+func InitDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	err = DB.AutoMigrate(&User{}, &UserPhoto{}, &Favorite{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+}
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func createUser(c *fiber.Ctx) error {
+	type Request struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var body Request
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	hash, _ := HashPassword(body.Password)
+
+	user := User{
+		ID:           uuid.New(),
+		Username:     body.Username,
+		Email:        body.Email,
+		PasswordHash: hash,
+		CreatedAt:    time.Now(),
+	}
+
+	if err := DB.Create(&user).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Could not create user"})
+	}
+
+	return c.JSON(user)
+}
+
+func getUsers(c *fiber.Ctx) error {
+	var users []User
+	if err := DB.Find(&users).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Could not get users"})
+	}
+	return c.JSON(users)
+}
+
 func main() {
+	InitDatabase()
+
 	app := fiber.New()
 
 	app.Get("/api/hello", func(c *fiber.Ctx) error {
@@ -14,5 +114,12 @@ func main() {
 		})
 	})
 
-	app.Listen("127.0.0.1:3000")
+	app.Post("/api/users", createUser)
+	app.Get("/api/users", getUsers)
+	app.Get("/api/users/:id", getOneUser)
+	app.Patch("/api/users/:id", updateUser)
+	app.Delete("/api/users/:id", deleteUser)
+
+	log.Println("Server is running on http://127.0.0.1:3000")
+	logrus.Fatal(app.Listen("127.0.0.1:3000"))
 }
